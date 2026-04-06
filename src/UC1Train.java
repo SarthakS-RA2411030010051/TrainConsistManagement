794984a798b168078c7d75db106ea8c284853266
SarthakS-RA2411030010051 <ss6883@srmist.edu.in>
2026-04-06 15:04:05 +0530

    Sarthak finished UC1-TrainConsistManagement

---
+++ src/UC1Train.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class UC1Train{
+    public static void main(String[] args){
+
+        System.out.println("=========================================================");
+        System.out.println("==============Train-Consist-Management-App===============");
+        System.out.println("=========================================================");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Initial Bogie Count : "+trainConsist.size());
+        System.out.println("Current Train Consist : "+trainConsist);
+
+        System.out.println("Train initialized successfully...");
+    }
+}
